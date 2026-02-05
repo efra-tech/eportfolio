c582cd8607ffdd935fd82ea0c72c018498f6639f
efra-tech <eahsan@uw.edu>
2026-02-05 13:29:24 -0800

    portfolio

--- eportfolio/src/components/Portfolio.js
+++ eportfolio/src/components/Portfolio.js
@@ -3,6 +3,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
+import carousel11 from '../imgs/AlbertsonsTest.png';
 import carousel1 from '../imgs/youFeed.png';
 import carousel2 from '../imgs/GardenSpace.png';
 import carousel3 from '../imgs/atlas.png';
@@ -16,6 +17,7 @@ import carouselX from '../imgs/winfo.png'
 import carouselY from '../imgs/LLC.png';
 import carouselA from '../imgs/nodejs.png';
 import carouselN from '../imgs/precious-pixie.png';
+import carouselAB from '../imgs/ixana.png';
 
 
 
@@ -29,6 +31,50 @@ export default function Portfolio(props) {
         <Carousel style={{backgroundColor: 'black', border: '8px solid black', borderRadius: '3rem', borderTop: '13px solid black'}}>
           <Carousel.Item style={{maxHeight: '650px'}}>
             <div style={{height: '650px'}}>
+              <a href='https://www.supermarketnews.com/grocery-technology/albertsons-to-test-veeve-smart-shopping-cart' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
+                className="d-block w-100"
+                src={carousel11}
+                alt="First slide"
+              /></a>
+            </div>
+            <Carousel.Caption>
+              <h5>
+                <em>"Albertsons</em>&nbsp;to test <em>Veeve</em>&nbsp;smart shopping cart"&nbsp;&nbsp;- Supermarket News&nbsp;
+                </h5>
+              <p className='pb-4 p'>Austin, Texas</p>
+              <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{maxHeight: '650px'}}>
+            <div style={{height: '650px'}}>
+              <a href='https://veeve.io/' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
+                className="d-block w-100"
+                src={carousel6}
+                alt="Fifth slide"
+              /></a>
+            </div>
+            <Carousel.Caption>
+              <h5 style={{marginBottom: '0px'}}><em>Veeve:</em>&nbsp;&nbsp;&nbsp;Smart Cart Start-Up</h5>
+              <p className='pb-4 p'>Austin, Texas</p>
+              <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{maxHeight: '650px'}}>
+            <div style={{height: '650px'}}>
+              <a href='https://www.ixana.ai/' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
+                className="d-block w-100"
+                src={carouselAB}
+                alt="Fifth slide"
+              /></a>
+            </div>
+            <Carousel.Caption>
+              <h5 style={{marginBottom: '0px'}}><em>10XAR:</em>&nbsp;&nbsp;&nbsp;Pioneer Technology</h5>
+              <p className='pb-4 p'>Seattle, Washington</p>
+              <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{maxHeight: '650px'}}>
+            <div style={{height: '650px'}}>
               <a href='https://gardenspace-muse.web.app/home' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
                 className="d-block w-100"
                 src={carousel2}
@@ -40,7 +86,7 @@ export default function Portfolio(props) {
                 <em>GardenSpace:</em>&nbsp;&nbsp;&nbsp;Global Open-Source Data and Software&nbsp;&nbsp;&nbsp;
                 <a rel="noreferrer" target="_blank" href="https://github.com/efra-tech/icapstone"><FontAwesomeIcon icon={faGithubSquare} style={{color: "white", height: "21.8px"}}/></a>
                 </h5>
-              <p className='pb-4 p'>The Information School at University of Washington</p>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -56,7 +102,7 @@ export default function Portfolio(props) {
               <h5><em>Synaptech @ UW:</em>&nbsp;&nbsp;&nbsp;Global Open-Source Project&nbsp;&nbsp;&nbsp;
                 <a rel="noreferrer" target="_blank" href="https://github.com/synaptech-uw/uwsynaptech-website"><FontAwesomeIcon icon={faGithubSquare} style={{color: "white", height: "21.8px"}}/></a>
               </h5>
-              <p className='pb-4 p'>University of Washington Organization</p>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -69,8 +115,8 @@ export default function Portfolio(props) {
               /></a>
             </div>
             <Carousel.Caption>
-              <h5><em>atlas:</em>&nbsp;&nbsp;&nbsp; Landing Page & Early Start-up</h5>
-              <p className='pb-4 p'>Annual Dempsey Start-Up Competition</p>
+              <h5><em>atlas:</em>&nbsp;&nbsp;&nbsp; Landing Page & Early Start-Up</h5>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -83,37 +129,8 @@ export default function Portfolio(props) {
               /></a>
             </div>
             <Carousel.Caption>
-              <h5><em>YouFeed:</em>&nbsp;&nbsp;&nbsp;Landing Page & Mobile Mockups</h5>
-              <p className='pb-4 p'>E2E Commercial Launch</p>
-              <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item style={{maxHeight: '650px'}}>
-            <div style={{height: '650px'}}>
-              <a href='https://veeve.io/' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
-                className="d-block w-100"
-                src={carousel6}
-                alt="Fifth slide"
-              /></a>
-            </div>
-            <Carousel.Caption>
-              <h5 style={{marginBottom: '0px'}}><em>Veeve, Inc:</em>&nbsp;&nbsp;&nbsp;Smart Design for AI-Powered Grocery</h5>
-              <p className='pb-4 p'>E2E Commercial Interface Design</p>
-              <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item style={{maxHeight: '650px'}}>
-            <div style={{height: '650px'}}>
-              <a href='https://precious-pixie-1d2d84.netlify.app/' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
-                className="d-block w-100"
-                src={carouselN}
-                alt="Sixth slide"
-              /></a>
-            </div>
-            <Carousel.Caption>
-              <h5><em>Interactive JavaScript:</em>&nbsp;&nbsp;&nbsp;Generate A Password&nbsp;&nbsp;&nbsp;
-              </h5>
-              <p className='pb-4 p'>Interactivity Fundamentals</p>
+              <h5><em>YouFeed:</em>&nbsp;&nbsp;&nbsp;Landing Page & Early Start-Up</h5>
+              <p className='pb-4 p'>Austin, Texas</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -127,7 +144,22 @@ export default function Portfolio(props) {
             </div>
             <Carousel.Caption>
               <h5><em>MexiKhana LLC:</em>&nbsp;&nbsp;&nbsp; Landing Page & Graphic Design</h5>
-              <p className='pb-4 p'>E2E Development & Design</p>
+              <p className='pb-4 p'>Portland, Oregon</p>
+              <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{maxHeight: '650px'}}>
+            <div style={{height: '650px'}}>
+              <a href='https://github.com/Info-443-Spring-2023/project-2-NodeJS.git' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
+                className="d-block w-100"
+                src={carouselA}
+                alt="Twelveth slide"
+              /></a>
+            </div>
+            <Carousel.Caption>
+              <h5><em>NodeJS:</em>&nbsp;&nbsp;&nbsp;Open-Source System Software Guide&nbsp;&nbsp;&nbsp;
+              <a rel="noreferrer" target="_blank" href="https://github.com/Info-443-Spring-2023/project-2-NodeJS"><FontAwesomeIcon icon={faGithubSquare} style={{color: "white", height: "21.8px"}}/></a></h5>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -155,7 +187,7 @@ export default function Portfolio(props) {
             </div>
             <Carousel.Caption>
               <h5><em>Dirt Masters, Inc:</em>&nbsp;&nbsp;&nbsp;IT & Business Consulting </h5>
-              <p className='pb-4 p'>Software Integrity</p>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -168,10 +200,10 @@ export default function Portfolio(props) {
               /></a>
             </div>
             <Carousel.Caption>
-              <h5><em>OurFamily:</em>&nbsp;&nbsp;&nbsp; Cloud Archive Application &nbsp;&nbsp;&nbsp;
+              <h5><em>OurFamily:</em>&nbsp;&nbsp;&nbsp; Cloud Archive Application&nbsp;&nbsp;&nbsp;
               <a rel="noreferrer" target="_blank" href="https://github.com/info340b-wi23/project-efra-tech"><FontAwesomeIcon icon={faGithubSquare} style={{color: "white", height: "21.8px"}}/></a>
               </h5>
-              <p className='pb-4 p'>Server-Side Development/SSR</p>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -184,24 +216,25 @@ export default function Portfolio(props) {
               /></a>
             </div>
             <Carousel.Caption>
-              <h5><em>Electric Vehicles in WA:</em>&nbsp;&nbsp;&nbsp;Interactive Data Analysis&nbsp;&nbsp;&nbsp;
+              <h5><em>Electric Vehicles in WA:</em>&nbsp;&nbsp;&nbsp;Interactive Data Analysis and App&nbsp;&nbsp;&nbsp;
               <a rel="noreferrer" target="_blank" href="https://github.com/info-201a-sp22/final-project-safjam?tab=readme-ov-file"><FontAwesomeIcon icon={faGithubSquare} style={{color: "white", height: "21.8px"}}/></a></h5>
-              <p className='pb-4 p'>Development and Deployment</p>
+              <p className='pb-4 p'>Seattle, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
+
           <Carousel.Item style={{maxHeight: '650px'}}>
             <div style={{height: '650px'}}>
-              <a href='https://github.com/Info-443-Spring-2023/project-2-NodeJS.git' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
+              <a href='https://precious-pixie-1d2d84.netlify.app/' target='_blank' rel='noreferrer'><img style={{borderRadius: '2.6rem', borderBottomLeftRadius: '0', border: '2px solid white'}}
                 className="d-block w-100"
-                src={carouselA}
-                alt="Twelveth slide"
+                src={carouselN}
+                alt="Sixth slide"
               /></a>
             </div>
             <Carousel.Caption>
-              <h5><em>NodeJS:</em>&nbsp;&nbsp;&nbsp;Open-Source Software Guide&nbsp;&nbsp;&nbsp;
-              <a rel="noreferrer" target="_blank" href="https://github.com/Info-443-Spring-2023/project-2-NodeJS"><FontAwesomeIcon icon={faGithubSquare} style={{color: "white", height: "21.8px"}}/></a></h5>
-              <p className='pb-4 p'>Software Architecture Project</p>
+              <h5><em>Interactive JavaScript:</em>&nbsp;&nbsp;&nbsp;Generate A Password&nbsp;&nbsp;&nbsp;
+              </h5>
+              <p className='pb-4 p'>Camas, Washington</p>
               <p style={{fontSize: '0.3rem'}}> &nbsp;</p>
             </Carousel.Caption>
           </Carousel.Item>
